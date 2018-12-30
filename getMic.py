71971edfbd021bf53f7d36fb724a82f385713012
BarelMeir <barel.meir@post.idc.ac.il>
2018-12-30 15:32:14 +0200

    mic1

--- getMic.py
+++ getMic.py
@@ -1,5 +1,4 @@
 import RPi.GPIO as GPIO
-import pygame
 from time import sleep
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
@@ -17,20 +16,6 @@ MOSI = 24
 CS = 25
 mcp = Adafruit_MCP3008.MCP3008(clk = CLK, cs = CS, miso = MISO, mosi = MOSI)
 
-#music load
-pygame.mixer.init()
-pygame.mixer.music.load("best_friend.mp3")
-pygame.mixer.music.play()
-
 while True:
 	print(mcp.read_adc(0))
 	sleep(2)
-
-# print("volume: ", pygame.mixer.music.get_volume())
-# sleep(1)
-# vol = 0.1
-# pygame.mixer.music.set_volume(vol)
-# print("volume: ", pygame.mixer.music.get_volume())
-# while pygame.mixer.music.get_busy() == True:
-# 	print(mcp.read_adc(0))
-# 	continue
