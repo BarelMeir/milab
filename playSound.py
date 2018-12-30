b5db2cb760aa29be009084f409d03b4b7d6c3753
BarelMeir <barel.meir@post.idc.ac.il>
2018-12-30 14:49:11 +0200

    vol change

--- playSound.py
+++ playSound.py
@@ -5,7 +5,7 @@ pygame.mixer.init()
 pygame.mixer.music.load("maron5.mp3")
 pygame.mixer.music.play()
 
-print("volume: " + pygame.mixer.music.get_volume())
+print("volume: ", pygame.mixer.music.get_volume())
 sleep(1)
 vol = 1
 pygame.mixer.music.set_volume(vol)
@@ -13,4 +13,4 @@ pygame.mixer.music.set_volume(vol)
 while pygame.mixer.music.get_busy() == True:
     vol-=0.1
     pygame.mixer.music.set_volume(vol)
-    print("volume: " + pygame.mixer.music.get_volume())
+    print("volume: " , pygame.mixer.music.get_volume())
