0e5953c212e8271bf485167d9d8fbdf9afd15f2a
BarelMeir <barel.meir@post.idc.ac.il>
2018-12-30 16:51:06 +0200

    Update playSound.py

--- playSound.py
+++ playSound.py
@@ -18,7 +18,7 @@ mcp = Adafruit_MCP3008.MCP3008(clk = CLK, cs = CS, miso = MISO, mosi = MOSI)
 
 #music load
 pygame.mixer.init()
-pygame.mixer.music.load("best_friend.mp3")
+pygame.mixer.music.load("maron5.mp3")
 pygame.mixer.music.play()
 
 print("volume: ", pygame.mixer.music.get_volume())
@@ -26,5 +26,5 @@ sleep(1)
 vol = 1
 pygame.mixer.music.set_volume(vol)
 
-while pygame.mixer.music.get_busy() == True:
+while True:
 	continue
